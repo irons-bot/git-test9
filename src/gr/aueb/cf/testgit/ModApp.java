b1f0a8cbd2afaab0f7158f49e70a73d3651c8aef
irotsak <iro_ns2@hotmail.com>
2025-12-03 20:48:49 +0000

    feat: add modapp

---
+++ src/gr/aueb/cf/testgit/ModApp.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.testgit;
+
+public class ModApp {
+
+    puclic static void main(String[] args) {
+
+    }
+
+    public static int mod(int a, int b) {
+        return a % b;
+    }
+
+}
